1c0fc2a412bcb6dc3feef74517d860453a9c3478
Josh Johnson <josh.johnson@captiveaire.com>
2010-10-14 10:22:01 -0400

    updated UI with BWToolkit controls, added framework for viewcontroller

---
+++ ClipboardAboutWindowController.mm
@@ -0,0 +1,14 @@
+//
+//  ClipboardAboutWindowController.m
+//  ClipboardManager
+//
+//  Created by Josh Johnson on 10/14/10.
+//  Copyright 2010 soda, LLC. All rights reserved.
+//
+
+#import "ClipboardAboutWindowController.h"
+
+
+@implementation ClipboardAboutWindowController
+
+@end
---
+++ ClipboardViewController.mm
@@ -0,0 +1,13 @@
+//
+//  ClipboardViewController.m
+//  ClipboardManager
+//
+//  Created by Josh Johnson on 10/14/10.
+//  Copyright 2010 soda, LLC. All rights reserved.
+//
+
+#import "ClipboardViewController.h"
+
+@implementation ClipboardViewController
+
+@end
